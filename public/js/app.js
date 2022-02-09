9640ff6f4f8f37d894c168067a8b54611cd11d8b
= <LHP6.arazzok@gmail.com>
2022-02-09 09:50:15 +0100

    add margin and padding details, add home section with a title

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body'),
+sidebar = document.querySelector('.sidebar'),
+toggle = document.querySelector('.toggle'),
+searchBtn = document.querySelector('.search-box'),
+modeSwitch = document.querySelector('.toggle-switch'),
+modeText = document.querySelector('.mode-text');
+
+toggle.addEventListener('click', () => sidebar.classList.toggle('close'));
+
+modeSwitch.addEventListener('click', () => {
+	body.classList.toggle('dark');
+	modeText.innerText = body.classList.contains('dark') ? 'Light Mode' : 'Dark Mode';
+});
